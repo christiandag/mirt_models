1e542b266706a2586d788ce641af4d2be0e09082
christiandag <christiandag2@gmail.com>
2021-11-07 22:44:10 -0500

    Remove unnecessary scripts

--- mod_ud_pcm_cri_script.R
+++
@@ -1,3 +0,0 @@
-mod <- readRDS("mod_ud_pcm_cri_script.rds")
-fscores(mod.pcm, method="EAP", full.scores=T, full.scores.SE = T, response.pattern=c(2,3,2,3,2,3,2,1,2,3,2,1,2,3,2,1,2,3,2,1,2,3,2,1,2,3,2,1,2,1,2,1,2,2,3,2,3,2,3,3))[41]
-
